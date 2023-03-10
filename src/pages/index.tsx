29cf4f1ed7fa9f5d727267c0ae8bc0a03103f4be
Nicolas Anaia <nicolasanaia@gmail.com>
2023-03-09 22:56:46 -0300

    adding global styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,14 @@
-import styles from '../styles/home.module.scss'
+import Head from 'next/head';
 
 export default function Home() {
   return (
-    <h1 className={styles.title}>
-      Testing <span>Sass</span>
-    </h1>
+    <>
+      <Head>
+        <title>Inicio | ig.news</title>
+      </Head>
+      <h1>
+        Testing
+      </h1>
+    </>
   )
 }
