99a75ba51e99e5e0dece78996e651922d593cf4b
Nicolas Anaia <nicolasanaia@gmail.com>
2023-03-28 18:56:54 -0300

    verifying in fauna if user is subscribed and storing email and name

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,11 +1,22 @@
+import { signIn, useSession } from 'next-auth/react'
 import { SubscribeButtonProps } from './interface'
 import styles from './styles.module.scss'
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+    const [session] = useSession();
+
+    function handleSubscribe() {
+        if (!session) {
+            signIn('github');
+            return;
+        }
+    }
+
     return (
         <button
             type='button'
             className={styles.subscribeButton}
+            onClick={handleSubscribe}
         >
             Subscribe Now
         </button>
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,13 +1,41 @@
+import { query as q } from 'faunadb';
+
 import NextAuth from 'next-auth';
+import { JWT } from 'next-auth/jwt';
 import GitHubProvider from "next-auth/providers/github";
+
+import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
     providers: [
         GitHubProvider({
             clientId: process.env.GITHUB_CLIENT_ID,
             clientSecret: process.env.GITHUB_CLIENT_SECRET,
-            scope: 'read:user',
+            scope: 'read:user'
         }),
     ],
+    callbacks: {
+        async signIn({ user, account, profile }) {
+            const { email, name } = user;
+
+            try {
+                await fauna.query(
+                    q.If(
+                        q.Not(
+                            q.Exists(
+                                q.Match(q.Index('user_by_email'), q.Casefold(user.email)),
+                            ),
+                        ),
+                        q.Create(q.Collection('users'), { data: { email, name } }),
+                        q.Get(q.Match(q.Index('user_by_email'), q.Casefold(user.email))),
+                    ),
+                );
+
+                return true;
+            } catch {
+                return false;
+            }
+        }
+    }
 });
 
